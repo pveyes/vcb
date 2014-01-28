c5bfe67e22dffe474d3416a289b65fc5d35eea18
Fatih Kalifa <fatih@pvey.es>
2014-01-27 22:20:46 -0800

    Convert handleGetOffer to WebRTC Modular design

--- src/js/vcb.js
+++ src/js/vcb.js
@@ -256,7 +256,7 @@ var registerSocket = function() {
 	 */
 
 	socket.on('ask-offer', handleAskOffer);
-	socket.on('get-offer', handleGetOffer);
+	socket.on('get-offer', webrtc.handleGetOffer);
 	socket.on('get-answer', webrtc.handleGetAnswer);
 	socket.on('get-last-description', webrtc.handleGetLastDescription);
 	socket.on('on-ice-candidate', webrtc.handleOnIceCandidate);
@@ -911,110 +911,6 @@ var handleAskOffer = function(d) {
 			}
 		};
 		socket.emit('get-offer', res);
-	});
-};
-
-var handleGetOffer = function(d) {
-	if (d.data.client.sessionID != socket.socket.sessionid) {
-		// Send notification to clients
-		// New client join
-		var joinMessage = $('#join-room-message-template').html().replace('{{name}}', d.data.client.name)
-		// Append to chat list
-		$('#chat-list').prepend(joinMessage)            
-	}
-
-	console.log('Info: handleGetOffer with input param @d:'); console.log(d);
-	webrtc.peers[d.data.from] = new webrtc.peerConstructor();
-	// Create RTCPeerConnection
-	webrtc.peers[d.data.from].peer = new webkitRTCPeerConnection(null,{optional: [{RtpDataChannels: true}]});
-	var peer = webrtc.peers[d.data.from].peer;
-
-	peer.addStream(stream.local);
-	
-	// Handle event on RTCPeerConnection
-	peer.onicecandidate = function(e){
-		console.log('Info: onicecandidate send from this PC to '+d.data.from); console.log(e);
-		var tmpCandidate = {
-			candidate:e.candidate.candidate, 
-			sdpMLineIndex:e.candidate.sdpMLineIndex, 
-			sdpMid:e.candidate.sdpMid
-		};
-		var res = {
-			status:true,
-			data:{
-				//from:this.id,
-				to:d.data.from,
-				candidate:tmpCandidate
-			}
-		};
-		socket.emit('on-ice-candidate',res);
-	};
-	peer.onsignalingstatechange = function(e){
-		console.log('Info: onsignalingstatechange from '+d.data.from); console.log(e);
-	};
-	peer.oniceconnectionstatechange = function(e){
-		console.log('Info: oniceconnectionstatechange from '+d.data.from); console.log(e);
-	};
-	peer.onaddstream = function(e) {
-		console.log('Info: onaddstream from ', d); console.log(e);
-		webrtc.peers[d.data.from].remoteStream = e;
-
-		var newStreamURL = window.URL.createObjectURL(e.stream)
-		if (d.data.from == stream.roomInfo.creator.sessionID) {
-			initMainStream(newStreamURL);
-		}
-		else {
-			initNewStream(d.data.from, newStreamURL, false);
-		}
-	}
-
-	peer.onremovestream = function(e){
-		console.log('Info: onremovestream from '+d.data.from); console.log(e);
-	};
-	peer.ondatachannel = function(e) {
-		console.log('Info: ondatachannel from '+d.data.from); console.log(e);
-		webrtc.peers[d.data.from].datachannel = e.channel;
-		var channel = e.channel;
-		channel.onclose = function(e) {
-			console.log("getOffer peer DataChannelObject onclose from: ", d.data.from)
-			console.log(e)
-		}
-		channel.onerror = function(e) {
-			console.log("getOffer peer DataChannelObject onerror from: ", d.data.from)
-			console.log(e)
-		}
-		channel.onmessage = function(e) {
-			console.log("getOffer peer DataChannelObject onmessage from: ", d.data.from)
-			console.log(e);
-			dataChannel.receiveMessage(d, e);
-		}
-		channel.onopen = function(e) {
-			console.log("getOffer peer DataChannelObject onopen", d);
-			console.log(e)
-		}
-	};
-	// Process input SDP
-	var SessionDescription = new RTCSessionDescription({
-								sdp:d.data.session.sdp, 
-								type:d.data.session.type
-							});
-	webrtc.peers[d.data.from].session.remote = SessionDescription;
-	peer.setRemoteDescription(SessionDescription);
-	peer.createAnswer(function(e){
-		console.log('Info: createAnswer from this PC to '+d.data.from); console.log(e);
-		webrtc.peers[d.data.from].session.local = e;
-		var res = {
-			status:true,
-			data:{
-				//from:this.id,
-				to:d.data.from,
-				session:{
-					sdp:e.sdp,
-					type:e.type
-				}
-			}
-		};
-		socket.emit('get-answer',res);
 	});
 };
 
--- src/js/webrtc.js
+++ src/js/webrtc.js
@@ -75,6 +75,125 @@ var webrtc = (function() {
 	};
 
 	/**
+	 * handle getOffer
+	 */
+
+	webrtc.handleGetOffer = function(d) {
+		if (d.data.client.sessionID != socket.socket.sessionid) {
+			// Send notification to clients
+			// New client join
+			var joinMessage = $('#join-room-message-template').html().replace('{{name}}', d.data.client.name)
+			// Append to chat list
+			$('#chat-list').prepend(joinMessage)            
+		}
+
+		console.log('Info: handleGetOffer with input param @d:');
+		console.log(d);
+
+		// Create RTCPeerConnection
+		webrtc.peers[d.data.from] = new webrtc.peerConstructor();
+		webrtc.peers[d.data.from].peer = new webkitRTCPeerConnection(null,{optional: [{RtpDataChannels: true}]});
+		var peer = webrtc.peers[d.data.from].peer;
+
+		peer.addStream(stream.local);
+		
+		// Handle event on RTCPeerConnection
+		peer.onicecandidate = function(e){
+			console.log('Info: onicecandidate send from this PC to '+d.data.from); console.log(e);
+			var tmpCandidate = {
+				candidate: e.candidate.candidate, 
+				sdpMLineIndex: e.candidate.sdpMLineIndex, 
+				sdpMid: e.candidate.sdpMid
+			};
+			var res = {
+				status:true,
+				data:{
+					//from:this.id,
+					to: d.data.from,
+					candidate: tmpCandidate
+				}
+			};
+			socket.emit('on-ice-candidate',res);
+		};
+		peer.onsignalingstatechange = function(e) {
+			console.log('Info: onsignalingstatechange from: ', d.data.from);
+			console.log(e);
+		};
+		peer.oniceconnectionstatechange = function(e) {
+			console.log('Info: oniceconnectionstatechange from: ', d.data.from);
+			console.log(e);
+		};
+		peer.onaddstream = function(e) {
+			console.log('Info: onaddstream from: ', d);
+			console.log(e);
+			
+			webrtc.peers[d.data.from].remoteStream = e;
+			var newStreamURL = window.URL.createObjectURL(e.stream)
+
+			if (d.data.from == stream.roomInfo.creator.sessionID) {
+				stream.initCreator(newStreamURL);
+			}
+			else {
+				stream.initViewer(d.data.from, newStreamURL, false);
+			}
+		}
+		peer.onremovestream = function(e){
+			console.log('Info: onremovestream from: ', d.data.from);
+			console.log(e);
+		};
+		peer.ondatachannel = function(e) {
+			console.log('Info: ondatachannel from: ', d.data.from);
+			console.log(e);
+
+			webrtc.peers[d.data.from].datachannel = e.channel;
+			var channel = e.channel;
+
+			// Handle data channel event
+			channel.onclose = function(e) {
+				console.log("getOffer peer DataChannelObject onclose from: ", d.data.from)
+				console.log(e)
+			}
+			channel.onerror = function(e) {
+				console.log("getOffer peer DataChannelObject onerror from: ", d.data.from)
+				console.log(e)
+			}
+			channel.onmessage = function(e) {
+				console.log("getOffer peer DataChannelObject onmessage from: ", d.data.from)
+				console.log(e);
+				dataChannel.receiveMessage(d, e);
+			}
+			channel.onopen = function(e) {
+				console.log("getOffer peer DataChannelObject onopen", d);
+				console.log(e)
+			}
+		};
+
+		// Process input SDP
+		var SessionDescription = new RTCSessionDescription({
+									sdp:d.data.session.sdp, 
+									type:d.data.session.type
+								});
+		webrtc.peers[d.data.from].session.remote = SessionDescription;
+		peer.setRemoteDescription(SessionDescription);
+		peer.createAnswer(function(e){
+			console.log('Info: createAnswer from this PC to '+d.data.from); console.log(e);
+			webrtc.peers[d.data.from].session.local = e;
+			var res = {
+				status:true,
+				data:{
+					//from:this.id,
+					to:d.data.from,
+					session:{
+						sdp:e.sdp,
+						type:e.type
+					}
+				}
+			};
+			socket.emit('get-answer', res);
+		});
+	};
+
+	/**
 	 * handle getAnswer
 	 */
 
