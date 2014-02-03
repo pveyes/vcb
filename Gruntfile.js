ea4c1269c49851cbed7d6c120af61326ea2d6fbf
Fatih Kalifa <fatih@pvey.es>
2014-02-03 00:36:12 -0800

    Move screenfull.js to libs

--- Gruntfile.js
+++ Gruntfile.js
@@ -29,7 +29,6 @@ module.exports = function(grunt) {
 				files: {
 					'app/public/script.min.js': 
 					[ 'src/js/libs/*.js',
-					  'src/js/screenfull.js',
 					  'src/js/dataChannel.js',
 					  'src/js/chat.js',
 					  'src/js/presentation.js',
